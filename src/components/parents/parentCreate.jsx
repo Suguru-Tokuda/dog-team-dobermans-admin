fa0c7c7533065abd47d4d9e8ec3568132cfff171
Suguru Tokuda <sugurut@wolfram.com>
2019-10-16 12:54:45 -0500

    Fixed props

--- src/components/parents/parentCreate.jsx
+++ src/components/parents/parentCreate.jsx
@@ -29,9 +29,9 @@ class ParentCreate extends Component {
         const { url } = this.props;
         return (
             <React.Fragment>
-                <Route path={`${url}/initial-params`} render={(props) => <ParentInitialForm {...props} initialParams={this.state.initialParams} onShowLoading={this.props.onShowLoading.bind(this)} onDoneLoading={this.props.onDoneLoading.bind(this)} />} />
-                <Route path={`${url}/pictures`} render={(props) => <ParentPictureForm {...props} initialParams={this.state.initialParams} pictures={this.state.pictures} onShowLoading={this.props.onShowLoading.bind(this)} onDoneLoading={this.props.onDoneLoading.bind(this)} />} />
-                <Route path={`${url}/confirmation`} render={(props) => <ParentConfirmation {...props} initialParams={this.state.initialParams} pictures={this.state.pictures} onShowLoading={this.props.onShowLoading.bind(this)} onDoneLoading={this.props.onDoneLoading.bind(this)} />} />
+                <Route path={`${url}/initial-params`} render={(props) => <ParentInitialForm {...props} initialParams={this.state.initialParams} onShowLoading={this.props.onShowLoading.bind(this)} onDoneLoading={this.props.onDoneLoading.bind(this)} onToPictureBtnClicked={this.handleNextBtnClicked.bind(this)} />} />
+                <Route path={`${url}/pictures`} render={(props) => <ParentPictureForm {...props} initialParams={this.state.initialParams} pictures={this.state.pictures} onShowLoading={this.props.onShowLoading.bind(this)} onDoneLoading={this.props.onDoneLoading.bind(this)} onToConfirmBtnClicked={this.handleConfBtnClicked.bind(this)} />} />
+                <Route path={`${url}/confirmation`} render={(props) => <ParentConfirmation {...props} initialParams={this.state.initialParams} pictures={this.state.pictures} onShowLoading={this.props.onShowLoading.bind(this)} onDoneLoading={this.props.onDoneLoading.bind(this)} onCancelBtnClicked={this.handleCancelClicked} />} />
             </React.Fragment>
         );
     }
