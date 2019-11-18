65670efadbe10ee48939be061ca2ea2632b47eed
Suguru Tokuda <suguru.tokuda@gmail.com>
2019-11-18 11:29:11 -0600

    Introduction work in progress

--- src/components/aboutUs/introductionsEditor.jsx
+++ src/components/aboutUs/introductionsEditor.jsx
@@ -3,6 +3,7 @@ import AboutUsService from '../../services/aboutUsService';
 import { Link } from 'react-router-dom';
 import SortableIntroductionTableRows from './sortableIntoructionTableRows';
 import toastr from 'toastr';
+import $ from 'jquery';
 
 class IntroductionsEditor extends Component {
     state = {
@@ -41,48 +42,73 @@ class IntroductionsEditor extends Component {
 
     getSortableTable = () => {
         const { introductions } = this.state;
-        const thead = (
-            <thead>
-                <tr>
-                    <th colSpan="5%"></th>
-                    <th colSpan="20%">Title</th>
-                    <th colSpan="50%">Description</th>
-                    <th colSpan="25%">Picture</th>
-                </tr>
-            </thead>
-        );
-        const tbody = (
-            <tbody>
-                <SortableIntroductionTableRows 
-                    introductions={introductions}
-                    onDeleteBtnClicked={this.handleDeleteBtnClicked.bind(this)}
-                    onDeletePictureBtnClicked={this.handleDeletePictureBtnClicked.bind(this)}
-                    onImageChanged={this.handleImageChanged.bind(this)}
-                    onTitleChanged={this.handleTitleChanged.bind(this)}
-                    onDescriptionChanged={this.handleDescriptionChanged.bind(this)}
-                     />
-            </tbody>
-        );
+        if (introductions.length > 0) {
+            const thead = (
+                <thead>
+                    <tr>
+                        <th colSpan="5%"></th>
+                        <th colSpan="10%">Title</th>
+                        <th colSpan="40%">Description</th>
+                        <th colSpan="25%">Picture</th>
+                        <th colSpan="10%">Action</th>
+                    </tr>
+                </thead>
+            );
+            const tbody = (
+                <tbody>
+                    <SortableIntroductionTableRows 
+                        introductions={introductions}
+                        onDeleteBtnClicked={this.handleDeleteBtnClicked.bind(this)}
+                        onDeletePictureBtnClicked={this.handleDeletePictureBtnClicked.bind(this)}
+                        onImageChanged={this.handleImageChanged.bind(this)}
+                        uploadBtnClicked={this.handleUploadBtnClicked}
+                        onTitleChanged={this.handleTitleChanged.bind(this)}
+                        onDescriptionChanged={this.handleDescriptionChanged.bind(this)}
+                         />
+                </tbody>
+            );
+            return (
+                <div className="table-responsive">
+                    <table className="table">
+                        {thead}
+                        {tbody}
+                    </table>
+                </div>
+            );
+        } else {
+            return null;
+        }
+    }
+
+    handleUploadBtnClicked() {
+        $('#picture-upload').click();
     }
 
     handleDeleteBtnClicked = (index) => {
-
+        console.log(index);
     }
 
     handleDeletePictureBtnClicked = (index) => {
 
     }
 
-    handleImageChanged = (event, index) => {
-
+    handleImageChanged = (index, event) => {
+        console.log(index);
+        console.log(event);
     }
 
-    handleTitleChanged = (event, index) => {
+    handleTitleChanged = (index, event) => {
+        const { introductions } = this.state;
         const title = event.target.value;
+        introductions[index].title = title;
+        this.setState({ introductions });
     }
 
-    handleDescriptionChanged = (event, index) => {
+    handleDescriptionChanged = (index, event) => {
+        const { introductions } = this.state;
         const description = event.target.value;
+        introductions[index].description = description;
+        this.setState({ introductions });
     }
 
     handleSubmitBtnClicked = () => {
@@ -96,11 +122,12 @@ class IntroductionsEditor extends Component {
                     <h4>Introductions Editor</h4>
                 </div>
                 <div className="card-body">
-                    {this.getSortableTable}
+                    {this.getSortableTable()}
                 </div>
                 <div className="card-footer">
                     <Link className="btn btn-secondary" to="/about-us">Back</Link>
-                    <button type="submit" className="btn btn-primary" onClick={this.handleSubmitBtnClicked}>{this.getSubmitBtnLabel()}</button>
+                    <button type="button" className="btn btn-secondary ml-2">Undo</button>
+                    <button type="submit" className="btn btn-primary ml-2" onClick={this.handleSubmitBtnClicked}>{this.getSubmitBtnLabel()}</button>
                 </div>
             </div>
         );
--- src/components/aboutUs/sortableIntoructionTableRows.jsx
+++ src/components/aboutUs/sortableIntoructionTableRows.jsx
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import { SortableContainer, SortableElement } from 'react-sortable-hoc';
 import SortablePictureList from '../miscellaneous/sortablePictureList';
 
-const SortableItem = SortableElement(({value, index, myIndex, onDeleteBtnClicked, onDeletePictureBtnClicked, onTitleChanged, onDescriptionChanged, onImageChanged}) => {
+const SortableItem = SortableElement(({value, index, myIndex, onDeleteBtnClicked, onDeletePictureBtnClicked, onTitleChanged, onDescriptionChanged, uploadBtnClicked, onImageChanged}) => {
     let imageElement;
     const { picture, validations } = value;
-    if (typeof picture.url !== 'undefined') {
+    if (picture !== null && typeof picture.url !== 'undefined') {
         imageElement = (
             <React.Fragment>
                 <div className="row">
@@ -22,7 +22,7 @@ const SortableItem = SortableElement(({value, index, myIndex, onDeleteBtnClicked
                 </div>
             </React.Fragment>
         );
-    } else if (typeof picture === 'object' && typeof picture.url === 'undefined' && typeof picture.reference === 'undefined') {
+    } else if (picture !== null && typeof picture === 'object' && typeof picture.url === 'undefined' && typeof picture.reference === 'undefined') {
         const objectURL = URL.createObjectURL(value.picture);
         imageElement = (
             <React.Fragment>
@@ -43,29 +43,27 @@ const SortableItem = SortableElement(({value, index, myIndex, onDeleteBtnClicked
     } else if (picture === null) {
         imageElement = (
             <React.Fragment>
-                <label htmlFor="picture-upload" className="custom-file-upload">
-                    <i className="fa fa-cloud-upload"></i> Upload
-                </label>
-                <input id="picture-upload" type="file" accept="image/*" onChange={onImageChanged} />
+                <button type="button" className="btn btn-sm btn-success" onClick={uploadBtnClicked}><i className="fa fa-image"></i>{` Select Image`}</button>
+                <input id="picture-upload" type="file" accept="image/*" onChange={onImageChanged.bind(this, myIndex)} />
             </React.Fragment>
         );
     }
     return (
         <tr>
-            <td><i className="fa fa-bars"></i></td>
-            <td>
-                <input type="text" className="form-control" value={value.title} onChange={() => onTitleChanged.bind(this, myIndex)} />
+            <td colSpan="5%"><i className="fa fa-bars"></i></td>
+            <td colSpan="10%">
+                <input type="text" className="form-control" value={value.title} onChange={onTitleChanged.bind(this, myIndex)} />
                 {typeof validations.title !== 'undefined' && (
                     <span className="text-danger">{validations.title}</span>
                 )}
             </td>
-            <td>
-                <textarea className="form-control" value={value.description} onChange={() => onDescriptionChanged.bind(this, index)}></textarea>
+            <td colSpan="40%">
+                <textarea className="form-control" value={value.description} onChange={onDescriptionChanged.bind(this, myIndex)} rows="10" style={{resize: 'none'}}></textarea>
                 {typeof validations.description !== 'undefined' && (
                     <span className="text-danger">{validations.description}</span>
                 )}
             </td>
-            <td>
+            <td colSpan="25%">
                 <div className="row">
                     <div className="col-12">
                         {imageElement}
@@ -75,18 +73,17 @@ const SortableItem = SortableElement(({value, index, myIndex, onDeleteBtnClicked
                     <span className="text-danger">{validations.picture}</span>
                 )}
             </td>
-            <td>
+            <td colSpan="10%">
                 <button className="btn btn-sm btn-danger" onClick={() => onDeleteBtnClicked(myIndex)}>Delete</button>
             </td>
         </tr>
     );
 });
 
-const SortableList = SortableContainer(({items, validations, onDeleteBtnClicked, onDeletePictureBtnClicked, onImageChanged, onTitleChanged, onDescriptionChanged}) => {
+const SortableList = SortableContainer(({items, onDeleteBtnClicked, onDeletePictureBtnClicked, onImageChanged, uploadBtnClicked, onTitleChanged, onDescriptionChanged}) => {
     return (
         <React.Fragment>
             {items.map((value, index) => {
-                const validation = validations[index];
                 return <SortableItem 
                         key={`item-${index}`} 
                         myIndex={index} 
@@ -94,6 +91,7 @@ const SortableList = SortableContainer(({items, validations, onDeleteBtnClicked,
                         value={value} 
                         onDeleteBtnClicked={onDeleteBtnClicked.bind(this)} 
                         onDeletePictureBtnClicked={onDeletePictureBtnClicked.bind(this)}
+                        uploadBtnClicked={uploadBtnClicked}
                         onImageChanged={onImageChanged.bind(this)}
                         onTitleChanged={onTitleChanged.bind(this)}
                         onDescriptionChanged={onDescriptionChanged.bind(this)}
@@ -104,6 +102,11 @@ const SortableList = SortableContainer(({items, validations, onDeleteBtnClicked,
 });
 
 class SortableIntroductionTableRows extends Component {
+    constructor(props) {
+        super(props);
+        console.log(props);
+    }
+
     state = {
         introductions: this.props.introductions,
     };
@@ -122,20 +125,23 @@ class SortableIntroductionTableRows extends Component {
 
     onSortEnd = ({prevIndex, nextIndex}) => {
         const { introductions } = this.state;
-        const tempIntroduction = introductions[prevIndex];
-        introductions.splice(prevIndex, 1);
-        SortablePictureList.splice(nextIndex, 0, tempIntroduction);
-        this.props.onSortEnd(introductions);
+        if (introductions.length > 1) {
+            const tempIntroduction = introductions[prevIndex];
+            introductions.splice(prevIndex, 1);
+            SortablePictureList.splice(nextIndex, 0, tempIntroduction);
+            this.props.onSortEnd(introductions);
+        }
     }
 
     render() {
-        const { introductions, validations } = this.state;
+        const { introductions } = this.state;
         return <SortableList 
                 items={introductions}
                 onSortEnd={this.onSortEnd} 
                 onDeleteBtnClicked={this.props.onDeleteBtnClicked.bind(this)} 
                 onDeletePictureBtnClicked={this.props.onDeletePictureBtnClicked.bind(this)}
                 onImageChanged={this.props.onImageChanged.bind(this)}
+                uploadBtnClicked={this.props.uploadBtnClicked}
                 onTitleChanged={this.props.onTitleChanged.bind(this)}
                 onDescriptionChanged={this.props.onDescriptionChanged.bind(this)}
                 />;
