cfdf6bcf737ae5c9a5a637797f0ad574fa3cbd55
Suguru Tokuda <suguru.tokuda@gmail.com>
2020-01-19 19:01:19 -0600

    Fixed alignment

--- src/components/common/login.jsx
+++ src/components/common/login.jsx
@@ -45,45 +45,47 @@ class Login extends Component {
     render() {
         const { email, password, formSubmitted } = this.state;
         return (
-            <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col-md-8">
-                        <div className="card-group">
-                            <div className="card p-4">
-                                <div className="card-body">
-                                    <h1>Login</h1>
-                                    <p className="text-muted">Sign In to your account</p>
-                                    <div className="input-group mb-3">
-                                        <div className="input-group-prepend">
-                                            <span className="input-group-text">
-                                                <i className="fa fa-user"></i>
-                                            </span>
+            <div className="centered">
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <div className="col-md-8">
+                            <div className="card-group">
+                                <div className="card p-4">
+                                    <div className="card-body">
+                                        <h1>Dog Team Dobermans Admin Login</h1>
+                                        <p className="text-muted">Sign In to your account</p>
+                                        <div className="input-group mb-3">
+                                            <div className="input-group-prepend">
+                                                <span className="input-group-text">
+                                                    <i className="fa fa-user"></i>
+                                                </span>
+                                            </div>
+                                            <input className="form-control" type="text" placeholder="Email" vlaue={email} onChange={this.handleSetEmail} onKeyUp={this.handleSubmitByEnter} />
+                                            {(formSubmitted === true && email === '') && (
+                                                <React.Fragment>
+                                                    <br />
+                                                    <small className="text-danger">Enter email</small>
+                                                </React.Fragment>
+                                            )}
                                         </div>
-                                        <input className="form-control" type="text" placeholder="Email" vlaue={email} onChange={this.handleSetEmail} onKeyUp={this.handleSubmitByEnter} />
-                                        {(formSubmitted === true && email === '') && (
-                                            <React.Fragment>
-                                                <br />
-                                                <small className="text-danger">Enter email</small>
-                                            </React.Fragment>
-                                        )}
-                                    </div>
-                                    <div className="input-group mb-4">
-                                        <div className="input-group-prepend">
-                                            <span className="input-group-text">
-                                                <i className="fa fa-lock"></i>
-                                            </span>
+                                        <div className="input-group mb-4">
+                                            <div className="input-group-prepend">
+                                                <span className="input-group-text">
+                                                    <i className="fa fa-lock"></i>
+                                                </span>
+                                            </div>
+                                            <input className="form-control" type="password" value={password} placeholder="Password" onChange={this.handleSetPassword} onKeyUp={this.handleSubmitByEnter} />
+                                            {(formSubmitted === true && password === '') && (
+                                                <React.Fragment>
+                                                    <br />
+                                                    <small className="text-danger">Enter password</small>
+                                                </React.Fragment>
+                                            )}
                                         </div>
-                                        <input className="form-control" type="password" value={password} placeholder="Password" onChange={this.handleSetPassword} onKeyUp={this.handleSubmitByEnter} />
-                                        {(formSubmitted === true && password === '') && (
-                                            <React.Fragment>
-                                                <br />
-                                                <small className="text-danger">Enter password</small>
-                                            </React.Fragment>
-                                        )}
-                                    </div>
-                                    <div className="row">
-                                        <div className="col-6">
-                                            <button className="btn btn-primary px-4" type="button" onClick={this.handleLoginBtnClicked}>Login</button>
+                                        <div className="row">
+                                            <div className="col-6">
+                                                <button className="btn btn-primary px-4" type="button" onClick={this.handleLoginBtnClicked}>Login</button>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
