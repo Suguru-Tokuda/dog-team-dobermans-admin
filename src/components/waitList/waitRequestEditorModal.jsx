6bd4a658b8810ff79ec7773550764345de5b5551
Suguru Tokuda <suguru.tokuda@gmail.com>
2020-02-10 00:18:20 -0600

    Removed the directory

--- src/components/waitList/waitRequestEditorModal.jsx
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import toastr from 'toastr';
-import $ from 'jquery';
-import WaitListService from '../../services/waitListService';
-
-export default class WaitRequestEditorModal extends Component {
-    
-}
