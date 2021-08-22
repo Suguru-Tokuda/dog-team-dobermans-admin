745114843e7c7de2c901805e33ea758eaead4306
Suguru Tokuda <suguru.tokuda@gmail.com>
2021-08-22 12:25:22 -0500

    Added quicksort

--- src/services/sortService.js
+++ src/services/sortService.js
@@ -1,4 +1,5 @@
 import utilService from './utilService';
+import moment from 'moment';
 
 export default class SortService {
 
@@ -38,6 +39,99 @@ export default class SortService {
         } else {
             return array;
         }
+    }
+
+    static quickSort(array, key, sortDescending) {
+        this.quickSortExecute(array, key, 0, array.length - 1, sortDescending);
+        return array;
+    }
+
+    /* quick sort */
+    static quickSortExecute(array, key, left, right, sortDescending) {
+        if (left >= right)
+            return;
+
+        let pivot = this.getValue(array, key, Math.floor((left + right) / 2));
+        const index = this.partition(array, key, left, right, pivot, sortDescending);
+
+        this.quickSortExecute(array, key, left, index - 1, sortDescending);
+        this.quickSortExecute(array, key, index, right, sortDescending);
+    }
+
+    static partition(array, key, left, right, pivot, sortDescending) {
+        while (left <= right) {
+            let leftVal = this.getValue(array, key, left);
+            let rightVal = this.getValue(array, key, right);
+
+            if (!sortDescending) {
+                while (leftVal < pivot) {
+                    left++;
+                    leftVal = this.getValue(array, key, left);
+                }
+    
+                while (rightVal > pivot) {
+                    right--;
+                    rightVal = this.getValue(array, key, right);
+                }
+            } else {
+                while (leftVal > pivot) {
+                    left++;
+                    leftVal = this.getValue(array, key, left);
+                }
+    
+                while (rightVal < pivot) {
+                    right--;
+                    rightVal = this.getValue(array, key, left);
+                }
+            }
+
+            if (left <= right) {
+                this.swap(array, left, right);
+                left++;
+                right--;
+            }
+        }
+
+        return left;
+    }
+
+    static getValue(array, key, index) {
+        let value = key ? utilService.accessValue(array[index], key) : array[index];
+        const valueType = typeof value;
+
+        switch (valueType) {
+            case 'string':
+                // check if the string is date string
+                const isDate = moment(value, moment.ISO_8601, true).isValid();
+                if (isDate) {
+                    const dateValue = new Date(value);
+                    if (dateValue.toString() !== 'Invalid Date')
+                        value = dateValue;
+                } else {
+                    value = value.toLowerCase();
+                }
+                break;
+            case 'number':
+                // check if the value is float
+                if (isNaN(parseFloat(value)) === false) {
+                    value = parseFloat(value);
+                }
+                break;
+            case 'undefined':
+                value = '';
+                break;
+            default:
+                value = '';
+                break;
+        }
+
+        return value;
+    }
+
+    static swap(array, left, right) {
+        const temp = array[left];
+        array[left] = array[right];
+        array[right] = temp;
     }
 
     static sortDisplayedData(arr, key, sortData, startIndex, endIndex) {
