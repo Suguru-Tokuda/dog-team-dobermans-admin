ff05bc3325dd1ca2924f3f45ff09c42d9ec5bcda
Suguru Tokuda <suguru.tokuda@gmail.com>
2021-10-19 22:25:09 -0500

    Fixed the URL

--- src/services/sessionInfoService.js
+++ src/services/sessionInfoService.js
@@ -1,7 +1,7 @@
 export default class SessionInfoService {
     static getBaseUrlForAPI() {
         const prodOrigins = [
-            'dogteamdobermans-admin.web.app', 
+            'https://dogteamdobermans-admin.web.app', 
             'https://dogteamdobermans-admin.firebaseapp.com'
         ];
         const isProd = prodOrigins.indexOf(window.location.origin) !== -1;
