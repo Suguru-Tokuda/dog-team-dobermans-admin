e13d3b69af64eefd03715fbceb3d772528735944
Suguru Tokuda <suguru.tokuda@gmail.com>
2023-03-16 18:36:12 -0500

    Updated for the new url

--- src/services/sessionInfoService.js
+++ src/services/sessionInfoService.js
@@ -1,10 +1,7 @@
 export default class SessionInfoService {
     static getBaseUrlForAPI() {
-        const prodOrigins = [
-            'https://dogteamdobermans-admin.web.app', 
-            'https://dogteamdobermans-admin.firebaseapp.com'
-        ];
-        const isProd = prodOrigins.indexOf(window.location.origin) !== -1;
+        const prodOriginIdentifier = 'https://dogteamdobermans';
+        let isProd = window.location.origin.toLowerCase().indexOf(prodOriginIdentifier) !== -1;
         
         if (isProd)
             return "https://us-central1-dogteamdobermans.cloudfunctions.net/";
